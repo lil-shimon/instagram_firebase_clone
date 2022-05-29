a2373ab35f4ba83b58f99f817f9d11b61aa77631
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-29 22:32:34 +0900

    feat: 🎸 Sign up screen ui

--- src/main/java/com/example/instagram_firebase_clone/auth/SignupScreen.kt
+++ src/main/java/com/example/instagram_firebase_clone/auth/SignupScreen.kt
@@ -1,11 +1,89 @@
 package com.example.instagram_firebase_clone.auth
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.instagram_firebase_clone.IgViewModel
+import com.example.instagram_firebase_clone.R
 
 @Composable
 fun SignupScreen(navController: NavController, vm: IgViewModel) {
-    Text(text = "SignupScreen")
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .verticalScroll(
+                    rememberScrollState()
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val usernameState = remember { mutableStateOf(TextFieldValue()) }
+            val emailState = remember { mutableStateOf(TextFieldValue()) }
+            val passState = remember { mutableStateOf(TextFieldValue()) }
+
+            Image(
+                painter = painterResource(id = R.drawable.ig_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Signup",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") })
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") })
+            OutlinedTextField(
+                value = passState.value,
+                onValueChange = { passState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "SIGN UP")
+            }
+            Text(text = "Already a user? Go to login ->",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { }
+            )
+        }
+    }
 }
