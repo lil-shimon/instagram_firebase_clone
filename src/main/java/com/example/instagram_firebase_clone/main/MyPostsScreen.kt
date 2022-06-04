4de90518fb72c0fee56c6c9e4449e262a2399e36
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-04 21:06:29 +0900

    feat: 🎸 user image card

--- src/main/java/com/example/instagram_firebase_clone/main/MyPostsScreen.kt
+++ src/main/java/com/example/instagram_firebase_clone/main/MyPostsScreen.kt
@@ -1,14 +1,22 @@
 package com.example.instagram_firebase_clone.main
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.ModifierLocalReadScope
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -104,5 +112,27 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
-
+    Box(
+        modifier = Modifier.padding(top = 16.dp)
+            .clickable { onClick.invoke() }
+    ) {
+        UserImageCard(
+            userImage = imageUrl,
+            modifier = Modifier.padding(8.dp)
+                .size(80.dp)
+        )
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier.size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = com.example.instagram_firebase_clone.R.drawable.ic_add),
+                contentDescription = null,
+                modifier = Modifier.background(Color.Blue)
+            )
+        }
+    }
 }
